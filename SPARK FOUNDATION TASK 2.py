8a9e00ae381bc4e3e6eeb282cf20a24d7d3f7fe7
shaun420-20 <67545559+shaun420-20@users.noreply.github.com>
2020-12-06 00:10:43 +0530

    Add files via upload

---
+++ SPARK FOUNDATION TASK 2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+df.head(10)
+    
+mms = StandardScaler()
+mms.fit(df)
+normalized_data =mms.transform(df)
+normalized_data
+
+
+K = range(1,11)
+sum_of_squared_distance =[]
+for k in K :
+    km = KMeans(n_clusters = k, init = 'k-means++', 
+                    max_iter = 300, n_init = 10, random_state = 0)
+    km = km.fit(normalized_data)
+    sum_of_squared_distance.append(km.inertia_)
+plt.plot(K,sum_of_squared_distance , 'bx-')
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('elbow method for optimal k ') # Within cluster sum of squares
+plt.show()
+
+x = df.iloc[:, [0, 1, 2, 3]].values
+kmeans = KMeans(n_clusters = 3, init = 'k-means++',
+                max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+
+
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], 
+            s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], 
+            s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1],
+            s = 100, c = 'green', label = 'Iris-virginica')
+
+# Plotting the centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], 
+            s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
